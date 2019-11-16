3aafe81683921031f22d3c062b11926233e570d5
Vladimir <kozyrev-vova@mail.ru>
2019-11-16 19:38:35 +0300

    add friendsComponent

--- ChatProject/ClientApp/src/components/pages/friends/friends.component.ts
+++ ChatProject/ClientApp/src/components/pages/friends/friends.component.ts
@@ -1,15 +1,30 @@
-
-import { FriendDto } from '../../../dto/friends/FriendDto';
 import { Component } from '@angular/core';
+import { UserDto } from '../../../dto/user/UserDto';
+import { UserHttpService } from '../../../HttpServices/UserHttpService';
 
 @Component({
     selector: 'app-friends',
     templateUrl: './friends.component.html',
-  })
+})
 
 export class FriendsComponent {
-  friend: FriendDto = {
-      name: 'Max',
-      login: 'Max100500'
-  };
+  private readonly _userHttpService: UserHttpService;
+  public users: UserDto[];
+
+  public constructor(userHttpService: UserHttpService) {
+    this._userHttpService = userHttpService;
+    this.reloadUser();
+  }
+
+  private reloadUser(): void {
+    this._userHttpService.getUsers().subscribe(values => {
+      this.users = values;
+    });
+  }
+
+  public deleteUser(id: number): void {
+    this._userHttpService.removeUser(id).subscribe(() => {
+      this.reloadUser();
+    });
+  }
 }
