d3b4209dd2d84b2f82b5649e4bb9cf9a75018a41
Vladimir <kozyrev-vova@mail.ru>
2020-01-12 16:32:46 +0300

    fix: правки на странице чата

--- ChatProject/ClientApp/src/components/pages/ChatPage/ChatPage.component.ts
+++ ChatProject/ClientApp/src/components/pages/ChatPage/ChatPage.component.ts
@@ -5,45 +5,49 @@ import { ActivatedRoute } from '@angular/router';
 import { DatePipe } from '@angular/common';
 
 @Component({
-    selector: 'app-chat',
-    templateUrl: './ChatPage.component.html',
-    styleUrls: ['./ChatPage.component.scss'],
-    providers: [ChatHttpService, DatePipe]
+  selector: 'app-chat',
+  templateUrl: './ChatPage.component.html',
+  styleUrls: ['./ChatPage.component.scss'],
+  providers: [ChatHttpService, DatePipe]
 })
 
 export class ChatPageComponent {
-    private readonly _chatHttpService: ChatHttpService;
-    public message: MessageDto = {
-        messageId: 3,
-        dateSend: new Date(),
-        textMessage: 'message3'
-    };
-    public messages: MessageDto[] = [
-        this.message, this.message
-    ] ;
+  private readonly _chatHttpService: ChatHttpService;
+  public message: MessageDto = {
+    messageId: 3,
+    dateSend: new Date(),
+    textMessage: 'message3'
+  };
+  public messages: MessageDto[] = [
+    this.message, this.message
+  ];
 
 
-    public currChatId: number;
+  public currChatId: number;
 
-    public constructor(chatHttpService: ChatHttpService, route: ActivatedRoute) {
-        this._chatHttpService = chatHttpService;
+  public constructor(chatHttpService: ChatHttpService, route: ActivatedRoute) {
+    this._chatHttpService = chatHttpService;
 
-        route.params.subscribe(params => {
-            const _currChatId: number | undefined = params['id'] !== undefined
-            ? Number(params['id'])
-            : 0;
-            this.currChatId = _currChatId;
-            // this.reloadMessages();
-        });
-    }
+    route.params.subscribe(params => {
+      const _currChatId: number | undefined = params['id'] !== undefined
+        ? Number(params['id'])
+        : 0;
+      this.currChatId = _currChatId;
+      // this.reloadMessages();
+    });
+  }
 
-    private reloadMessages(): void {
-        this._chatHttpService.getMessages(this.currChatId).subscribe(values => {
-            this.messages = values;
-        });
-    }
+  private reloadMessages(): void {
+    this._chatHttpService.getMessages(this.currChatId).subscribe(values => {
+      this.messages = values;
+    });
+  }
 
-    public dateToString(date: Date): string {
-        return date.toLocaleString();
-    }
+  public sendMessage(): void {
+    console.log("Send message complete");
+  }
+
+  public dateToString(date: Date): string {
+    return date.toLocaleString();
+  }
 }
